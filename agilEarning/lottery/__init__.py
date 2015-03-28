a0e2efe2ccca2ab957220210f950a4e64c0c8748
c3h3 <c3h3.tw@gmail.com>
2015-03-28 22:48:05 +0800

    fetch data and create DataFrame

--- agilEarning/lottery/__init__.py
+++ agilEarning/lottery/__init__.py
@@ -1,2 +1,11 @@
+import pandas as pd
+
 def getHistoricalData(from_year=2004, to_year=2015, data_only=False):
-    pass
+    years = range(from_year, to_year)
+
+    get_hist_table = lambda year: pd.read_html("http://www.nfd.com.tw/lottery/49-year/49-%s.htm" % year)[0][1:]
+    hist_tables = map(get_hist_table,years)
+    
+    df = pd.concat(hist_tables)
+    df.columns = ["year","monthDay","YN","x1","x2","x3","x4","x5","x6","s","TN"]
+    return df
