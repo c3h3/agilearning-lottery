7f299268fc06e2a6011428c305c401a958a812a6
c3h3 <c3h3.tw@gmail.com>
2015-03-29 21:32:55 +0800

    finished data_only=True

--- agilEarning/tests/test_datasets.py
+++ agilEarning/tests/test_datasets.py
@@ -10,5 +10,10 @@ def test_fn_getHistoricalData():
     assert all(df.columns == pd.Index(["YN","TN","year","monthDay","x1","x2","x3","x4","x5","x6","s"], dtype='object'))
     assert df.shape == (1154, 11)
     
+    df = getHistoricalData(from_year=2004, to_year=2015, data_only=True)
+    
+    assert isinstance(df, pd.DataFrame)
+    assert all(df.columns == pd.Index(["x1","x2","x3","x4","x5","x6","s"], dtype='object'))
+    assert df.shape == (1154, 7)
     
     
